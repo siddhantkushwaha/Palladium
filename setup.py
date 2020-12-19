f298bc8dc0c555982eceef8f4deafcadde199200
siddhantkushwaha <k16.siddhant@gmail.com>
2020-12-19 15:03:12 +0530

    upgrading the version

--- setup.py
+++ setup.py
@@ -8,15 +8,16 @@ README = (HERE / "README.md").read_text()
 setup(
     name='palladium-python',
     packages=['palladium'],
-    version='0.1.4',
+    version='0.1.5',
     license='MIT',
     description='Common utility functions.',
     author='Siddhant Kushwaha',
     author_email='k16.siddhant@gmail.com',
     url='https://github.com/siddhantkushwaha/palladium',
-    download_url='https://github.com/siddhantkushwaha/palladium/archive/0.1.4.tar.gz',
+    download_url='https://github.com/siddhantkushwaha/palladium/archive/0.1.5.tar.gz',
     keywords=['CHROMIUM', 'SELENIUM', 'AUTOMATION', 'TESTING'],
     install_requires=[
+        'pandas',
         'requests',
         'selenium',
         'python-dateutil',
